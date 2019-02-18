fa03ef5708850693251470553ddd6a965a813777
stbaeumer <stefan.baeumer@berufskolleg-borken.de>
2019-02-18 14:34:20 +0100

    ...

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 })
 
 app.get('/',(req, res, next) => {   
-
-        res.render('index.ejs', {                    
+    res.render('index.ejs', {                    
     })
 })
 
