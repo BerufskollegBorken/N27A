e4f1f42193c03e59c2d4c4ff211fa63fa33fb5d2
Stu <stbaeumer@gmail.com>
2019-03-02 07:27:25 +0100

    ...

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
 app.get('/',(req, res, next) => {   
-    res.render('index.ejs', {                    
+
+    let idKunde = req.cookies['istAngemeldetAls']
+
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {                    
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
+
+app.get('/login',(req, res, next) => {       
+    res.render('login.ejs', {                    
     })
+})
+
+app.post('/',(req, res, next) => {   
+    res.render('index.ejs', {                    
+    })    
 })
 
