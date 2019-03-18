44572529e549b4395c002c7f3d1d570ef35b91dd
Stu <stbaeumer@gmail.com>
2019-03-18 10:28:05 +0100

    ...

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
-
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
@@ -16,10 +14,11 @@ const server = app.listen(process.env.PORT || 3000, () => {
 app.get('/',(req, res, next) => {   
 
     let idKunde = req.cookies['istAngemeldetAls']
-
+    
     if(idKunde){
         console.log("Kunde ist angemeldet als " + idKunde)
         res.render('index.ejs', {                    
+            user : idKunde
         })
     }else{
         res.render('login.ejs', {                    
@@ -28,12 +27,31 @@ app.get('/',(req, res, next) => {
 })
 
 app.get('/login',(req, res, next) => {       
+  
+    res.cookie('istAngemeldetAls', '')        
+    
     res.render('login.ejs', {                    
     })
 })
 
 app.post('/',(req, res, next) => {   
-    res.render('index.ejs', {                    
-    })    
+    
+    const idKunde = req.body.idKunde
+    const kennwort = req.body.kennwort
+        
+    if(idKunde === "4711" && kennwort === "123"){            
+        console.log("Der Cookie wird gesetzt:")
+        res.cookie('istAngemeldetAls', idKunde)
+        res.render('index.ejs', { 
+
+            // 11.3.
+            user : idKunde
+        })
+    }else{            
+        console.log("Der Cookie wird gelöscht")
+        res.cookie('istAngemeldetAls','')
+        res.render('login.ejs', {                    
+        })
+    }
 })
 
